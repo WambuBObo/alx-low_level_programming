8a7568aaba208487e6a3ecef62cf7a991c8b37e7
Wambu <wggachora@gmail.com>
2022-03-13 13:56:16 -0700

    Print out all lowercase letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: Prints out all lowercase letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	
+	}
+
+	putchar('\n');
+	return (0);
+}
+
