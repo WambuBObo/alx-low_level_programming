db304e2e7bb6abce363e47ff6adedb667c5fff70
Wambu <wggachora@gmail.com>
2022-03-15 16:32:20 -0700

    Print character to standard output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Definitions: Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	-putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Prints character to standard output
