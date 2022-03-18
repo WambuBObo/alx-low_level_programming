107d87727f537c7be6d9249de5836a80e35d003c
Wambu <wggachora@gmail.com>
2022-03-17 17:29:04 -0700

    Prints numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers,  to 9
+ * @void: no return
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; 1 <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
