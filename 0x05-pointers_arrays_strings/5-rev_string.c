7dd09f489e3628beaa248fca42f615bba02fcf77
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-03-22 02:36:31 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,54 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-
-	int length = 0;
-
-	while (*s){
-		s++;
-		length++;}
-	return length;
-}
-
-/**
- * rev_string - reverses a string
- * @s: string s
- */
+* rev_string - reverse a string
+*@s: string to reverse
+*Return: Nothing
+*/
 void rev_string(char *s)
 {
+	int len, i;
+	char l;
 
-	int length = _strlen(s);
-	char *clone = s;
-	char *tmp = malloc((length + 1) * sizeof(char));
-
-	tmp += length;
-
-	while (*clone)
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	len--;
+	for (i = 0; i < len; i++)
 	{
-		*tmp = *clone;
-		printf("%p,value-->%s, copied-->%s\n",tmp,clone,tmp);
-		tmp--;
-		clone++;
-
+		l = *(s + len);
+		*(s + len) = *(s + i);
+		*(s + i) = l;
+		len--;
 	}
-	tmp+=1;
-	//printf("before--%s",s);
-	printf("TMP-->%s,s-->%s\n",tmp,s);
-	*s = *tmp;
-}
-
-int main(void)
-{
-	char str[] = "five";
-	rev_string(str);
-	printf("done");
-	printf("%s\n",str);
-	return (0);
 }
