054dafc856e15d5a098c923f8c01356d0726c3f2
Wambu <wggachora@gmail.com>
2022-03-21 16:46:04 -0700

    Print one  char out of two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,10 @@ void puts2(char *str)
 
 	for (len = 0; *(str + len) != 0; len++)
 	{
-		if (len %2 == 0)
+		if (len % 2 == 0)
 		{		l = *(str + len);
 				_putchar(l);
 		}
 	}
 	_putchar('\n');
 }
-	
-
