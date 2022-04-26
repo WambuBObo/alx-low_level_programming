4dbee7017da459bb29231150933f0bd58d2b41a6
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-04-26 03:08:45 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,40 +1,23 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * print_listint - print elements of linkedlist
+ * @h: linked list
  *
- * Return: Always 0.
+ * Return: number of nodes
  */
-int main(void)
+
+size_t print_listint(const listint_t *h)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
+	size_t nbr_nodes = 0;
+	const listint_t *current = h;
 
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		nbr_nodes++;
+	}
+	return (nbr_nodes);
 }
