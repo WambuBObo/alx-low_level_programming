7887bf75206ba809ce8a16ee8a8c293de29ccf7f
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-04-29 03:59:07 +0300

    Update 0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,63 +1,68 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * powX - raises b to p
- * @b: base
- * @p: power
- * Return: b the power of p
- */
-int powX(int b, int p)
-{
-	int prod = 1;
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+*/
 
-	while (p > 0)
-	{
-		prod *= b;
-		p--;
-	}
-	return (prod);
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
 }
+
 
 /**
- * _len - length of a string
- * @s:string
- * Return: lenght of s
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
-int _len(const char *s)
-{
-	int len = 0;
 
-	while (*s)
-	{
-		len++;
-		s++;
-	}
-	return (len);
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+	return (-1);
+else if (y == 1)
+	return (x);
+else if (y == 0)
+	return (1);
+
+return (x * _pow_recursion(x, y - 1));
+
 }
+
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: input string
- * Return: unsigned decimal conversion of input is NULL
+ * @b: binary number
+ *
+ * Return: 0 or converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int power = _len(b) - 1;
-	int num = 0;
+	unsigned int n = 0;
+	int i, len;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	while (*b)
+	len = _strlen(b);
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] == '1')
+			n += _pow_recursion(2, (len - i - 1));
+		else if (b[i] != '0')
 			return (0);
-
-
-		if (*b == '1')
-			num += powX(2, power);
-
-		b++;
-		power--;
 	}
-	return (num);
+
+	return (n);
 }
