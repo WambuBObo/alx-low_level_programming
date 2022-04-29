2824ec3ab53d53486637606181bddbe42e25659c
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-04-29 04:00:03 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define  BIT_SIZE 8
+#define BYTE_LENGHT 8
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -14,8 +10,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
-int _strlen(const char *s);
-_Bool isBinary(char);
 
 #endif
