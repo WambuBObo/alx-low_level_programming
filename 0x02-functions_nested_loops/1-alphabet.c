58dc838dc0404688af5ab0860593062ac6f8861b
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2023-01-11 15:45:27 +0300

    Create 1-alphabet.c
    function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Print lower case alphabet
+ *
+ * void: function takes no arguments
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
